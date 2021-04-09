e16ce485a2f4cec01310e801aa89e9e5095b1be9
jpablodavid <jpablodavid1307@gmail.com>
2021-04-09 15:42:52 -0300

    responsividade pagina home

--- src/script.js
+++ src/script.js
@@ -1,12 +1,37 @@
 $(document).ready(() => {
+    //menu
+    $(".navbar__menu-btn").on('click', function () {
+        $('.navbar__links').toggleClass('active');
+        $(this).find('i').toggleClass('fa-bars');
+        $(this).find('i').toggleClass('fa-times');
+    });
+
+    //carrosel
     const slickOptions = {
         autoplay: true,
         autoplaySpeed: 2000,
         dots: false,
+        //pauseOnHover: false,
         prevArrow: '<button type="button" class="slick-prev slider__prev-arrow">Previous</button>;',
         nextArrow: '<button type="button" class="slick-next slider__next-arrow">Nextvious</button>;'    
     };
     $('.slider').slick(slickOptions);
+
+    const counterOptions = {
+        delay: 10,
+        time: 1000,
+    };
+
+    setInterval(() => {
+        const numbersCollection = document.querySelectorAll(".counter__number");
+
+        numbersCollection.forEach((number) => {
+            const cuurentNumber = parseInt(number.innerHTML);
+            number.innerHTML = cuurentNumber + 1;
+        });
+    }, 1000);
+
+    $(".counter__number").counterUp(counterOptions);
 
     $('.footer__form-button').on('click', () => {
         const email = $("#email").val();
